8a57e3b9d63467caa52a93389761b2af905818f0
Naveed Mughal <naveedahmadmughal063@gmail.com>
2026-01-12 10:09:47 +0500

    Minor changes

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -3,7 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-
+use App\Http\Resources\SubCategoryResource;
 class ProductResource extends JsonResource
 {
     public function toArray($request): array
